6f1800e132bc823c272b5781a708aa645aa7dcac
David Taylor <david@taylorhq.com>
2024-03-15 15:06:03 +0000

    FIX: Simplify sidebar custom link implementation
    All our link validation, and conversion from url -> route/model/query is expensive and prone to bugs. Instead, if people enter a link, we can just use it as-is.
    
    Originally all this extra logic was added to handle unusual situations like `/safe-mode`, `/my/...`, etc. However, all of these are now handled correctly by our Ember router, so there is no need for it.
    
    Now, we just pass the user-supplied `href` directly to the SectionLink component, and let Ember handle routing to it when clicked.
    
    The only functional change here is that we no longer validate internal links by parsing them with the Ember router. But I'd argue this is fine, because the previous logic would cause both false positives (e.g. `/t/123` would be valid, even if topic 123 doesn't exist), and false negatives (for routes which are server-side only, like the new AI share pages).

--- app/assets/javascripts/discourse/app/lib/sidebar/section-link.js
+++ app/assets/javascripts/discourse/app/lib/sidebar/section-link.js
@@ -1,15 +1,9 @@
 import { tracked } from "@glimmer/tracking";
-import RouteInfoHelper from "discourse/lib/sidebar/route-info-helper";
-import { defaultHomepage } from "discourse/lib/utilities";
 
 export default class SectionLink {
   @tracked linkDragCss;
 
-  constructor(
-    { external, full_reload, icon, id, name, value },
-    section,
-    router
-  ) {
+  constructor({ external, full_reload, icon, id, name, value }, section) {
     this.external = external;
     this.fullReload = full_reload;
     this.prefixValue = icon;
@@ -19,19 +13,6 @@ export default class SectionLink {
     this.value = value;
     this.section = section;
     this.withAnchor = value.match(/#\w+$/gi);
-
-    if (!this.externalOrFullReload) {
-      const routeInfoHelper = new RouteInfoHelper(router, value);
-
-      if (routeInfoHelper.route === "discovery.index") {
-        this.route = `discovery.${defaultHomepage()}`;
-      } else {
-        this.route = routeInfoHelper.route;
-      }
-
-      this.models = routeInfoHelper.models;
-      this.query = routeInfoHelper.query;
-    }
   }
 
   get shouldDisplay() {
